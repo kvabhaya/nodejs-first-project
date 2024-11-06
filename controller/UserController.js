bb7feb6dc53c161191ebcbc71d4a4a5fa2e8bc1a
kvabhaya <kvabhayasundara16@gmail.com>
2024-11-07 00:09:51 +0530

    modified login to user

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,5 +1,8 @@
 const UserSchema = require('../model/UserSchema');
 const bcrypt = require('bcrypt');
+const wt = require('jsonwebtoken');
+
+const secret = process.env.SECRET;
 
 const signup = async (req, resp) => {
     // console.log(req.body);
@@ -25,6 +28,34 @@ const signup = async (req, resp) => {
             .catch(error => resp.status(500).json({
                 'message': 'Something went wrong', error
             }))
+    } catch (e) {
+        resp.status(500).json({
+            'message': 'Something went wrong', error: e
+        })
+    }
+}
+const login = async (req, resp) => {
+    // console.log(req.body);
+    try {
+
+        const existingUser = await UserSchema.findOne({email: req.body.email});
+
+        if(!existingUser){
+            return resp.status(404).json({'message': 'User not found'});
+        }
+
+        const isConfirmed = await bcrypt.compare(req.body.password,existingUser.password);
+
+        if(!isConfirmed){
+            return resp.status(401).json({'message': 'Password is wrong'});
+        }
+
+        const token = wt.sign({userId:existingUser._id,
+                        email:existingUser.email,
+                        fullName:existingUser.fullName},
+            secret, {expiresIn: '5h'});
+
+        resp.status(200).json({'token': token, 'message':'user logged'});
     } catch (e) {
         resp.status(500).json({
             'message': 'Something went wrong', error: e
@@ -114,4 +145,4 @@ const signup = async (req, resp) => {
 //     }
 // }
 
-module.exports = {signup}
+module.exports = {signup, login}
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController=require('../controller/UserController');
 
 router.post('/signup',userController.signup);
+router.post('/login',userController.login);
 // router.get('/find/:id',customerController.findOneById);
 // router.delete('/delete/:id',customerController.deleteOneById);
 // router.put('/update/:id',customerController.updateById);
