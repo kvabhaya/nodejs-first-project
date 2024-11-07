3c3436cc883015d4e9bc5add2e9cdf3ef9d6c163
kvabhaya <kvabhayasundara16@gmail.com>
2024-11-07 23:44:46 +0530

    added middleware.js

--- routes/CustomerRoute.js
+++ routes/CustomerRoute.js
@@ -1,12 +1,13 @@
 const express=require('express');
 const router = express.Router();
 const customerController=require('../controller/CustomerController');
+const verifyToken=require('../middleware/Middleware');
 
-router.post('/create',customerController.create); //http://localhost:3000/customer
-router.get('/find/:id',customerController.findOneById);
-router.delete('/delete/:id',customerController.deleteOneById);
-router.put('/update/:id',customerController.updateById);
-router.get('/search',customerController.search);
+router.post('/create',verifyToken,customerController.create); //http://localhost:3000/customer
+router.get('/find/:id',verifyToken,customerController.findOneById);
+router.delete('/delete/:id',verifyToken,customerController.deleteOneById);
+router.put('/update/:id',verifyToken,customerController.updateById);
+router.get('/search',verifyToken,customerController.search);
 
 module.exports = router;
 
